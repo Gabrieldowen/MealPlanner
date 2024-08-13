57e483051053e7a5b1023dd8a9cd5ca97fa456c9
Gabe <Gabriel.d.owen-1@ou.edu>
2024-08-13 15:34:17 -0500

    add functionality to search by keyword for recipes and basic dispaly

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 1. set up flask
 2. add functionality to search for recipies using api
+    - https://developer.edamam.com/edamam-recipe-api
 3. save ingrediants to a grocery list
 4. add functinoality for a whole week of meals
 5. add filters for budget
+
+The app can work like an online shopping site. You specify your nutrition goals then shop for food that looks good. As you add meals the counter displaying calories and macros go up.
--- app.py
+++ app.py
@@ -1,10 +1,44 @@
-from flask import Flask, render_template
-
+from flask import Flask, render_template, request, redirect, url_for
+from config import Config
+import requests
 app = Flask(__name__)
 
-@app.route('/')
+# API endpoints
+RECIPE_FINDER_URL = 'https://api.edamam.com/api/recipes/v2'
+
+# initialize the app
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    
+    recipes = []
+    if request.method == 'POST':
+
+        # parameters for the API request
+        params = {
+            'type': 'public',
+            'q': request.form.get('query'),
+            'app_id': Config.RECIPE_FINDER_ID,
+            'app_key': Config.RECIPE_FINDER_KEY,
+        }
+        
+        response = requests.get(RECIPE_FINDER_URL, params=params)
+        
+        # if the request is successful, the recipes will be displayed
+        if response.status_code == 200:
+            data = response.json()
+            recipes = data.get('hits')
+
+        # if there are no recipes, the user will be redirected to the success page
+        else:
+            print('Error:', response.status_code, response.text)
+            recipes = []
+        
+    return render_template('index.html', recipes=recipes)
+
+# if the form is submitted successfully, the user will be redirected to the success page
+@app.route('/success')
+def success():
+    return "Form submitted successfully!"
 
 if __name__ == '__main__':
     app.run(debug=True)
